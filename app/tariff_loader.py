8e6d4aabcf0bf4337a52f5c29b1f346b9d7da188
meyap <mmeyiapir@gmail.com>
2023-07-16 03:11:28 +0400

    First attempt

--- app/tariff_loader.py
+++ app/tariff_loader.py
@@ -5,7 +5,4 @@ def load_tariff_from_file(filename):
         tariff = json.load(file)
     return tariff
 
-# Загрузка тарифа через API
-# def load_tariff_from_api():
-#     # ваш код для загрузки тарифа из API
-#     pass
+
